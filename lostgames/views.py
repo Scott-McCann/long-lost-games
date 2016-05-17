43b9c3c481f8105806437d8e829befe34cae23e0
Scott McCann <scottmccann@live.com>
2016-05-17 15:57:13 -0500

    added details page

--- lostgames/views.py
+++ lostgames/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Game
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 # Create your views here.
 def index(request):
     games = Game.objects.all()
@@ -10,3 +10,13 @@ def index(request):
     context = {'games': games}
 
     return HttpResponse(templ.render(context, request))
+
+def view_game(request, game_id):
+
+    try:
+        game = Game.objects.get(id=game_id)
+    except game.DoesNotExist:
+        raise Http404("No such Game!")
+    context = { 'game': game }
+
+    return render(request, 'lostgames/game_details.html', context)
