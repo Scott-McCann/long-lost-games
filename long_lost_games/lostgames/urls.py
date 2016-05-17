1329f66f06b784579bde5d432991f87aaaca74fe
Scott McCann <scottmccann@live.com>
2016-05-17 15:57:13 -0500

    added details page

--- long_lost_games/lostgames/urls.py
+++ long_lost_games/lostgames/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.conf.urls import url
-
+from .models import Game
 urlpatterns = [
-    url(r'^$', views.index, name='games-index')
+    url(r'^$', views.index, name='games-index'),
+    url(r'^(?P<game_id>[0-9]+)/$', views.view_game, name='content-view_game')
+
 ]
