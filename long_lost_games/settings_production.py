2a1737fe823f1f179c2c4c26fefcde499f7d0fa3
Scott McCann <scottmccann@live.com>
2016-06-01 12:48:05 -0500

    Database settings and heroku config

---
+++ long_lost_games/settings_production.py
@@ -0,0 +1,22 @@
+from .settings import *
+import dj_database_url
+
+DEBUG = False
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
